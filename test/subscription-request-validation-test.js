935d5dc01f58768983bddc533f795e56d62c438e
David Björklund <david.bjorklund@gmail.com>
2013-05-11 14:54:06 +0200

    [fix] remove nested tests, use tape instead of tap

--- test/subscription-request-validation-test.js
+++ test/subscription-request-validation-test.js
@@ -1,74 +1,70 @@
 var http = require('http')
   , qs = require('querystring')
-  , test = require('tap').test
+  , test = require('tape')
   , common = require('./common')
   , request = require('request')
 
-test('pubhub', function(t) {
-  t.test('setup', common.setup.bind(common))
+test('setup', common.setup.bind(common))
 
-  t.test('subscription request', function(t) {
-    t.plan(4)
+test('subscription request', function(t) {
+  t.plan(4)
 
-    common.hubRequest(
-        {
-            body: qs.stringify(
-                {
-                    'hub.mode': 'subscribe'
-                  , 'hub.topic': common.topicUrl
-                }
-            )
-        }
-      , function(err, res, data) {
-          t.notEqual(res.statusCode, 200, 'hub.callback is required')
-        }
-    )
+  common.hubRequest(
+      {
+          body: qs.stringify(
+              {
+                  'hub.mode': 'subscribe'
+                , 'hub.topic': common.topicUrl
+              }
+          )
+      }
+    , function(err, res, data) {
+        t.notEqual(res.statusCode, 200, 'hub.callback is required')
+      }
+  )
 
-    common.hubRequest(
-        {
-            body: qs.stringify(
-                {
-                    'hub.callback': common.callbackUrl
-                  , 'hub.topic': common.topicUrl
-                }
-            )
-        }
-      , function(err, res, data) {
-          t.notEqual(res.statusCode, 200, 'hub.mode is required')
-        }
-    )
+  common.hubRequest(
+      {
+          body: qs.stringify(
+              {
+                  'hub.callback': common.callbackUrl
+                , 'hub.topic': common.topicUrl
+              }
+          )
+      }
+    , function(err, res, data) {
+        t.notEqual(res.statusCode, 200, 'hub.mode is required')
+      }
+  )
 
-    common.hubRequest(
-        {
-            body: qs.stringify(
-                {
-                    'hub.mode': 'subscribe'
-                  , 'hub.callback': common.callbackUrl
-                }
-            )
-        }
-      , function(err, res, data) {
-          t.notEqual(res.statusCode, 200, 'hub.topic is required')
-        }
-    )
+  common.hubRequest(
+      {
+          body: qs.stringify(
+              {
+                  'hub.mode': 'subscribe'
+                , 'hub.callback': common.callbackUrl
+              }
+          )
+      }
+    , function(err, res, data) {
+        t.notEqual(res.statusCode, 200, 'hub.topic is required')
+      }
+  )
 
-    common.hubRequest(
-        {
-            body: qs.stringify(
-                {
-                    'hub.mode': 'wrong'
-                  , 'hub.callback': common.callbackUrl
-                  , 'hub.topic': common.topicUrl
-                }
-            )
-        }
-      , function(err, res, data) {
-          t.notEqual(res.statusCode, 200, 'hub.mode should be subscribe or unsubscribe')
-        }
-    )
-  })
-
-  t.test('teardown', common.teardown.bind(common))
-
-  t.end()
+  common.hubRequest(
+      {
+          body: qs.stringify(
+              {
+                  'hub.mode': 'wrong'
+                , 'hub.callback': common.callbackUrl
+                , 'hub.topic': common.topicUrl
+              }
+          )
+      }
+    , function(err, res, data) {
+        t.notEqual(res.statusCode, 200, 'hub.mode should be subscribe or unsubscribe')
+      }
+  )
 })
+
+test('teardown', common.teardown.bind(common))
